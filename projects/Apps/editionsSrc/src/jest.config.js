4834265cb9322e9b2bb5cada7e30382389122390
James Miller <james.miller.freelancer@guardian.co.uk>
2019-11-21 17:04:11 +0000

    chore: pre React Navigation

--- projects/Apps/editionsSrc/src/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    preset: 'react-native',
-    transformIgnorePatterns: ['/node_modules/(?!(@guardian|react-native))/'],
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-    setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
-    testMatch: ['**/__tests__/**/?(*.)+(spec|test).[jt]s?(x)'],
-}
