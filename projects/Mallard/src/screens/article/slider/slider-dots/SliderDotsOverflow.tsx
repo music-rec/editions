c2041a775d2734db86f68c4d8b0291bff2ab6a53
James Miller <james.miller.freelancer@guardian.co.uk>
2020-03-16 11:12:26 +0000

    chore: Forwards and backwards WIP

--- projects/Mallard/src/screens/article/slider/slider-dots/SliderDotsOverflow.tsx
+++ projects/Mallard/src/screens/article/slider/slider-dots/SliderDotsOverflow.tsx
@@ -45,9 +45,11 @@ const SliderDotsOverflow = React.memo(
                 ? Animated.subtract(position, startIndex)
                 : position
 
+        const positionAsNumber = Number(JSON.stringify(newPos))
+
         const largeDeviceMemory = useLargeDeviceMemory()
-        const range = (i: number) =>
-            largeDeviceMemory
+        const range = (i: number) => {
+            return largeDeviceMemory
                 ? {
                       inputRange: [
                           i - 0.50000000001,
@@ -62,8 +64,104 @@ const SliderDotsOverflow = React.memo(
                       inputRange: [i - 1, i, i + 1],
                       outputRange: ['#DCDCDC', color, '#DCDCDC'],
                   }
+        }
 
-        // console.log(newPos)
+        console.log(dotsAllowed - 2)
+
+        const interpolationForward = (i: number) => {
+            if (i < dotsAllowed - 3) {
+                return newPos.interpolate({
+                    ...range(i),
+                    extrapolate: 'clamp',
+                })
+            } else if (i === dotsAllowed - 3) {
+                return newPos.interpolate({
+                    inputRange: [
+                        dotsAllowed - 3.50000000001,
+                        dotsAllowed - 3.5,
+                        dotsAllowed,
+                        numOfItems - 2.5,
+                        numOfItems - 2.49999999999,
+                    ],
+                    outputRange: ['#DCDCDC', color, color, color, '#DCDCDC'],
+                    extrapolate: 'clamp',
+                })
+            } else if (i === dotsAllowed - 2) {
+                const value = numOfItems - 2
+                return newPos.interpolate({
+                    inputRange: [
+                        value - 0.50000000001,
+                        value - 0.5,
+                        value,
+                        value + 0.5,
+                        value + 0.50000000001,
+                    ],
+                    outputRange: ['#DCDCDC', color, color, color, '#DCDCDC'],
+                    extrapolate: 'clamp',
+                })
+            } else if (i === dotsAllowed - 1) {
+                const value = numOfItems - 1
+                return newPos.interpolate({
+                    inputRange: [
+                        value - 0.50000000001,
+                        value - 0.5,
+                        value,
+                        value + 0.5,
+                        value + 0.50000000001,
+                    ],
+                    outputRange: ['#DCDCDC', color, color, color, '#DCDCDC'],
+                    extrapolate: 'clamp',
+                })
+            }
+        }
+
+        const interpolationBackward = (i: number) => {
+            if (i > 2) {
+                return newPos.interpolate({
+                    ...range(i),
+                    extrapolate: 'clamp',
+                })
+            }
+            // } else if (i === 2) {
+            //     return newPos.interpolate({
+            //         inputRange: [
+            //             dotsAllowed - 3.50000000001,
+            //             dotsAllowed - 3.5,
+            //             dotsAllowed,
+            //             numOfItems - 2.5,
+            //             numOfItems - 2.49999999999,
+            //         ],
+            //         outputRange: ['#DCDCDC', color, color, color, '#DCDCDC'],
+            //         extrapolate: 'clamp',
+            //     })
+            // } else if (i === 1) {
+            //     const value = numOfItems - 2
+            //     return newPos.interpolate({
+            //         inputRange: [
+            //             value - 0.50000000001,
+            //             value - 0.5,
+            //             value,
+            //             value + 0.5,
+            //             value + 0.50000000001,
+            //         ],
+            //         outputRange: ['#DCDCDC', color, color, color, '#DCDCDC'],
+            //         extrapolate: 'clamp',
+            //     })
+            // } else if (i === 0) {
+            //     const value = numOfItems - 1
+            //     return newPos.interpolate({
+            //         inputRange: [
+            //             value - 0.50000000001,
+            //             value - 0.5,
+            //             value,
+            //             value + 0.5,
+            //             value + 0.50000000001,
+            //         ],
+            //         outputRange: ['#DCDCDC', color, color, color, '#DCDCDC'],
+            //         extrapolate: 'clamp',
+            //     })
+            // }
+        }
 
         for (let i = 0; i < dotsAllowed; i++) {
             const backgroundColor =
@@ -71,10 +169,9 @@ const SliderDotsOverflow = React.memo(
                     ? i === newPos
                         ? color
                         : '#DCDCDC'
-                    : newPos.interpolate({
-                          ...range(i),
-                          extrapolate: 'clamp',
-                      })
+                    : direction === 'forward'
+                    ? interpolationForward(i)
+                    : interpolationBackward(i)
 
             const smallerDotStyle =
                 (direction === 'forward' && i == dotsAllowed - 2) ||
