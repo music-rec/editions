945903e8150b58f054a4aeef081dd8369aadf0f4
Philip McMahon <philip.mcmahon@guardian.co.uk>
2020-05-27 18:06:35 +0100

    Remove unused import

--- projects/Mallard/src/authentication/webview-modal.tsx
+++ projects/Mallard/src/authentication/webview-modal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Modal, View } from 'react-native'
 import WebView from 'react-native-webview'
 import { WebViewNavigation } from 'react-native-webview/lib/WebViewTypes'
