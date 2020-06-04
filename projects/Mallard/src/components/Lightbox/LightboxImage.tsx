7c84fac1a78287aee26b38e158e78200159a329a
Aoife McLaughlin <aoife.mclaughlin@guardian.co.uk>
2020-06-04 12:22:01 +0100

    add pinch gesture to zoom in

--- projects/Mallard/src/components/Lightbox/LightboxImage.tsx
+++ projects/Mallard/src/components/Lightbox/LightboxImage.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Image, StyleSheet } from 'react-native'
+import { Image, StyleSheet, Animated } from 'react-native'
 import { Image as IImage } from '../../../../Apps/common/src'
 import { useImagePath } from 'src/hooks/use-image-paths'
+import { PinchGestureHandler } from 'react-native-gesture-handler'
+import { transform } from '@babel/core'
 
 const styles = StyleSheet.create({
     image: {
@@ -13,14 +15,18 @@ const styles = StyleSheet.create({
 })
 
 const LightboxImage = ({ image }: { image: IImage }) => {
+    const scale = React.useRef(new Animated.Value(1)).current
     const imagePath = useImagePath(image, 'full-size')
+    const handlePinch = Animated.event([{ nativeEvent: { scale } }])
     return (
-        <Image
-            style={styles.image}
-            source={{
-                uri: imagePath,
-            }}
-        />
+        <PinchGestureHandler onGestureEvent={handlePinch}>
+            <Animated.Image
+                style={[styles.image, { transform: [{ scale }] }]}
+                source={{
+                    uri: imagePath,
+                }}
+            />
+        </PinchGestureHandler>
     )
 }
 
