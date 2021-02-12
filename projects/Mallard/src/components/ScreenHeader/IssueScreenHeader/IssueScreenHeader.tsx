064ee5e472941c45bdd5c986b7f98cf5fec1cc15
Anna Beddow <anna.beddow@gmail.com>
2021-02-12 15:49:54 +0000

    Update Title and Subtitle of issue header

--- projects/Mallard/src/components/ScreenHeader/IssueScreenHeader/IssueScreenHeader.tsx
+++ projects/Mallard/src/components/ScreenHeader/IssueScreenHeader/IssueScreenHeader.tsx
@@ -10,6 +10,12 @@ import { IssueMenuButton } from '../../Button/IssueMenuButton'
 import { EditionsMenuButton } from '../../EditionsMenu/EditionsMenuButton/EditionsMenuButton'
 import { ScreenHeader } from '../ScreenHeader'
 import { useEditions } from 'src/hooks/use-edition-provider'
+
+interface Titles {
+    title: string
+    subTitle: string
+}
+
 const IssueScreenHeader = withNavigation(
     ({
         headerStyles,
@@ -21,20 +27,36 @@ const IssueScreenHeader = withNavigation(
     } & NavigationInjectedProps) => {
         const { date, weekday } = useIssueDate(issue)
 
+        const getDateString = () => {
+            const abbreviatedDay = weekday.substring(0, 3)
+            return `${abbreviatedDay} ${date}`
+        }
+
+        const { setNewEditionSeen, selectedEdition } = useEditions()
+
         const goToIssueList = () => {
             navigateToIssueList(navigation)
         }
-        const { setNewEditionSeen } = useEditions()
 
         const handleEditionMenuPress = () => {
             setNewEditionSeen()
             navigateToEditionMenu(navigation)
         }
+        
+        const getTitles = (): Titles => {
+            if (selectedEdition.editionType !== 'Special') {
+                const dateString = getDateString()
+                return { title: selectedEdition.title, subTitle: dateString }
+            }
+            const splitTitle = selectedEdition.title.split('\n')
+            return { title: splitTitle[0], subTitle: splitTitle[1] }
+        }
 
+        const titles = getTitles()
         return (
             <ScreenHeader
-                title={weekday}
-                subTitle={date}
+                title={titles.title}
+                subTitle={titles.subTitle}
                 onPress={goToIssueList}
                 rightAction={<IssueMenuButton onPress={goToIssueList} />}
                 leftAction={
